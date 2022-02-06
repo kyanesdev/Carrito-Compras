6d893e2ffd41566d2316d63dee6029c50cdba32d
kyanesdev <kyanesdev@gmail.com>
2022-02-06 16:45:26 -0300

    imagenes acomodadas

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Productos from './components/Productos'
 import Layout from './components/Layout'
 import Title from './components/Title'
 import Navbar from './components/Navbar'
+import Tomate from './images/tomate.jpg'
+import Arbejas from './images/arbejas.jpg'
+import Lechuga from './images/lechuga.jpg'
 class App extends Component {
 
   state = {
     productos: [
-      { name: 'Tomate' , price: 1500 , img: '/images/tomate.jpg' },
-      { name: 'Arbejas' , price: 2500 , img: '/images/arbejas.jpg' },
-      { name: 'Lechuga' , price: 500 , img: '/images/lechuga.jpg' },
+      { name: 'Tomate' , price: 1500 , img: Tomate },
+      { name: 'Arbejas' , price: 2500 , img: Arbejas },
+      { name: 'Lechuga' , price: 500 , img: Lechuga },
     ],
 
     carro: [],
